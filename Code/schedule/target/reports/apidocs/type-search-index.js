774c3856a6f28731537c8b882ebf5e950c469a27
Руслан <schischanov.ruslan@yandex.ru>
2024-12-15 19:20:38 +0300

    clean

--- Code/schedule/target/reports/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"App"},{"p":"org.example","l":"MatrixToCSV"},{"p":"org.example","l":"Parsers"},{"p":"org.example","l":"PredictAlgorithm"},{"p":"org.example","l":"TimeSeriesData"}];updateSearchResults();
